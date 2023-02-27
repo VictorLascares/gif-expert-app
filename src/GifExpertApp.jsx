67896680e40804762a989768da487696ad12a8c5
Victor Lascares <vmlascares@gmail.com>
2023-02-26 20:48:34 -0600

    Agregando una categoria a lista de categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -3,12 +3,16 @@ import { useState } from "react";
 const GifExpertApp = () => {
     const [categories, setCategories] = useState(['Demon Slayer', 'Dragon ball z']);
 
+    const onAddCategory = () => {
+        const categoria = prompt('Ingresa una cateogoria');
+        setCategories([...categories, categoria]);
+    };
 
     return (
         <>
             <h1>GifExpertApp</h1>
 
-
+            <button onClick={ onAddCategory }>Agregar</button>
             <ol>
                 {
                     categories.map(category => <li key={ category } >{ category }</li>)
