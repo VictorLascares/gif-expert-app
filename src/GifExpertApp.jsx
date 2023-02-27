d0bb1e909a2ab264d321a1bdcfd147d3ca6c69ba
Victor Lascares <vmlascares@gmail.com>
2023-02-26 20:33:14 -0600

    Creando hook de categorias y mostrando las categorias en un listado

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['Demon Slayer', 'Dragon ball z']);
+
+
     return (
         <>
             <h1>GifExpertApp</h1>
+
+
+            <ol>
+                {
+                    categories.map(category => <li key={ category } >{ category }</li>)
+                }
+            </ol>
         </>
     );
 };
