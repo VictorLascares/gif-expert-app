cc8ffe91d27cceeacf957df68a0290d7e2dd96be
Victor Lascares <vmlascares@gmail.com>
2023-03-04 21:44:50 -0600

    Comprobando que el hook useFetchGifs retorne una arreglo de gifs e isLoading este en false

--- tests/hooks/useFetchGifs.test.js
+++ tests/hooks/useFetchGifs.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { useFetchGifs } from "../../src/hooks/useFetchGifs";
 
 describe('Probando hook useFetchGifs', () => {
@@ -8,6 +8,17 @@ describe('Probando hook useFetchGifs', () => {
 
         expect( gifs ).toHaveLength(0);
         expect( isLoading ).toBeTruthy();
+    });
 
+    test('Debe de retornar un arreglo de images e isLoading en false', async () => {
+        const { result } = renderHook(() => useFetchGifs('One Punch'));
+        await waitFor(
+            () => expect( result.current.gifs.length ).toBeGreaterThan(0)
+        );
+
+        const { gifs, isLoading } = result.current;
+
+        expect( gifs.length ).toBeGreaterThan(0);
+        expect( isLoading ).toBeFalsy();
     });
 });
